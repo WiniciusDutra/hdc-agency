7e9a77c24e20c78a842f601ee8fa08d48755d567
Vinícius Dutra de Aguiar <winicius.aguiar@gmail.com>
2024-11-21 20:29:21 -0300

    como usar o input do Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent} from './pages/home/home.component';
 import { UsersComponent } from './pages/users/users.component';
 import { UserComponent } from './pages/user/user.component';
+import { UserCardComponent } from './_components/user-card/user-card.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'users', component: UsersComponent},
-  {path: 'users/details', component: UserComponent}
+  {path: 'users/details', component: UserComponent},
+  {path: 'users/usercard', component: UserCardComponent}
 
 ];
 
--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { User } from '../../_models/user';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
+
+
 export class UsersComponent {
 
-  listaUsuarios: string[] = ['Vinicius', 'Simone', 'Malú', 'Olaf', 'Catarina']
+
+  //listaUsuarios: string[] = ['Vinicius', 'Simone', 'Malú', 'Olaf', 'Catarina']
+
+  usersList: User[] = 
+  [
+    { name: 'Vinícius', age: 36},
+    { name: 'Simone', age: 36},
+    { name: 'Maria Luisa', age: 5},
+    { name: 'Maria Rodrigues', age: 65},
+    { name: 'Valney', age: 38},
+    { name: 'Orácio', age: .1}
+    
+  ]
 
 }
